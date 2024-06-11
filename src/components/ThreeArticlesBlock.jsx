5f18a67664bb54487190fae9aee3cc57133243c2
Emanuele <emanuelesquillante@libero.it>
2024-06-11 14:04:53 +0200

    risoluzione bug

--- src/components/ThreeArticlesBlock.jsx
+++ src/components/ThreeArticlesBlock.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchTopStories } from './FetchTopStories';
-import '../style/ThreeAB.css' 
+import '../style/ThreeAB.css'
 
 const ThreeArticlesBlock = ({ section }) => {
   const [articles, setArticles] = useState([]);
@@ -11,7 +11,7 @@ const ThreeArticlesBlock = ({ section }) => {
     const loadTopStories = async () => {
       try {
         const data = await fetchTopStories(section);
-        setArticles(data.slice(0, 3)); 
+        setArticles(data.slice(0, 3));
       } catch (error) {
         setError(error.message);
       } finally {
@@ -30,9 +30,9 @@ const ThreeArticlesBlock = ({ section }) => {
       <div className="top-articles">
         {articles.slice(0, 2).map(article => (
           <div key={article.url} className="top-article">
-            <h2>{article.title}</h2>
-            <p>{article.abstract}</p>
-            <a href={article.url}  target='_blank'>Read more</a>
+            <a href={article.url}  target='_blank'><h2>{article.title}</h2></a>
+            <a href={article.url}  target='_blank'><p>{article.abstract}</p></a>
+            <a href={article.url} target='_blank'>Read more</a>
             <div className="article-meta">
               <span>{article.byline}</span>
             </div>
@@ -42,20 +42,20 @@ const ThreeArticlesBlock = ({ section }) => {
       {articles[2] && (
         <div className="bottom-article">
           <div className="article-content">
-            <h2>{articles[2].title}</h2>
-            <p>{articles[2].abstract}</p>
-            <a href={articles[2].url} target='_blank'>Read more</a>
-            <div className="article-meta">
-              <span>{articles[2].byline}</span> 
-            </div>
+            <a href={articles[2].url} target='_blank'><h2>{articles[2].title}</h2></a>
+              <a href={articles[2].url} target='_blank'><p>{articles[2].abstract}</p></a>
+                <a href={articles[2].url} target='_blank'>Read more</a>
+                <div className="article-meta">
+                  <span>{articles[2].byline}</span>
+                </div>
+              </div>
+              {articles[2].multimedia && articles[2].multimedia[0] && (
+                <a href={articles[2].url}  target='_blank'><img src={articles[2].multimedia[0].url} alt={articles[2].title} /></a>
+              )}
           </div>
-          {articles[2].multimedia && articles[2].multimedia[0] && (
-            <img src={articles[2].multimedia[0].url} alt={articles[2].title} />
-          )}
-        </div>
       )}
-    </div>
-  );
+        </div>
+      );
 };
 
-export default ThreeArticlesBlock;
+      export default ThreeArticlesBlock;
